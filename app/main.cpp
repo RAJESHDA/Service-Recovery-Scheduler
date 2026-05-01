1c8fcb7fb3eceac24671df8da57843fb348f2ba4
RAJESHDA <raj.venki15@gmail.com>
2026-05-01 09:52:54 +0530

    Project/Excercise Folder structure with Cmake support

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+﻿// Service-Recovery-Scheduler.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello from app." << endl;
+	return 0;
+}
